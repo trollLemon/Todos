9fea960997a86a3fcd27d9e3c314b01a5c09df2e
haydn <haydnt@zohomail.com>
2022-09-10 11:52:21 -0700

    demo of the yaml generation, also added an enum helper function

--- src/file.cpp
+++ src/file.cpp
@@ -1,3 +1,67 @@
 #include "file.h"
 #include "types.h"
-#include "yaml.h"
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+
+  //helper function to return strings based on the status Enum in types.h
+    std::string status( todoStatus stat)
+    {
+
+        switch (stat)
+        {
+            case todoStatus::notStarted:
+                return "Not Started";
+            case todoStatus::inProgress:
+                return "In Progress";
+            case todoStatus::done:
+                return "Done";
+            default:
+                return "Err";
+        }
+
+    }
+
+
+
+
+
+namespace datastream {
+
+    std::vector<Todo> readFile(std::string& path)
+    {
+        std::vector<Todo> data{};
+        return data;
+    }
+    void writeFile(std::vector<Todo> todos, std::string path)
+    {
+        YAML::Emitter output; //this will output our data into our data file
+        
+        for (Todo& todo: todos)
+        {
+            output << YAML::BeginMap;
+
+            output << YAML::Key   << "Title";
+            output << YAML::Value << todo.getName();
+            output << YAML::Key   << "Status";
+            output << YAML::Value << status(todo.getStatus());
+            
+            //now loop through all our tasks
+            for (Task& task : todo.getTasks())
+            {
+                output << YAML::Key   << task.getName();
+                output << YAML::Value << YAML::BeginSeq 
+                                      << task.getContent()
+                                      << task.getStatus()
+                                      << YAML::EndSeq;
+            }
+            output << YAML::EndMap; 
+        }
+
+        std::cout << "Here's the output YAML:\n" << output.c_str();
+
+    }
+    void createBackup()
+    {
+
+    }
+}

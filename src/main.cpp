42b343db6ee4a92435fc1262686373318c0cf403
haydn <haydnt@zohomail.com>
2022-09-11 11:44:09 -0700

    new tests

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,23 @@ int main()
    
     std::vector<Todo> todos;
     std::string path {"/home/haydn/Github/TodoList/test/test.yaml"};
-    Todo test = Todo("A Todo");
-    Task t = Task("Sus", "    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+    Todo test = Todo("A");
+    Todo test2 = Todo("Todo");
+    Todo test3 = Todo("test");
+    
+    Task t = Task("test", "Test code for bugs");
+    Task y = Task("test", "There are bugs in the code");
 
     test.addTask(t);
-    test.addTask(t);
+    test2.addTask(t);
+    test2.addTask(y);
+    test3.addTask(t);
 
     todos.push_back(test);
-    todos.push_back(test);
+    todos.push_back(test2);
+    todos.push_back(test3);
     datastream::writeFile(todos, path); 
-
+    datastream::readFile(path);
     return 0;
 }
 

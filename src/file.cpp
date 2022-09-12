e2a5a4f67ab461f1e54e8928122ab6043b821fe0
haydn <haydnt@zohomail.com>
2022-09-11 17:56:42 -0700

    loads data into a vector of Todo objects

--- src/file.cpp
+++ src/file.cpp
@@ -21,7 +21,18 @@
         }
 
     }
+    
+    //helper to return an enum based on a string
 
+    todoStatus stringToEnum(std::string string)
+    {
+        if(string == "Not Started")
+            return todoStatus::notStarted;
+        else if (string== "In Progress")
+            return todoStatus::inProgress;
+        else
+            return todoStatus::done;
+    }
 
 
 
@@ -30,44 +41,67 @@ namespace datastream {
 
     std::vector<Todo> readFile(std::string& path)
     {
-        std::ifstream file(path);
-        YAML::Parser parser(file);
-
+        YAML::Node file = YAML::LoadFile(path);
         std::vector<Todo> data{};
+        
+        for(int i{0}; i< file.size(); ++i)
+        {
+            std::string todoTitle = file[i][i].as<std::string>();
+            todoStatus enumStatus = stringToEnum(file[i]["status"].as<std::string>());           
+            Todo newTodo = Todo(todoTitle,enumStatus);
+            
+            for(int j{0}; j < file[i]["data"].size(); ++j)
+            {
+                std::string title = file[i]["data"][j]["name"].as<std::string>();
+                std::string content = file[i]["data"][j]["content"].as<std::string>();  
+                std::string done = file[i]["data"][j]["isDone"].as<std::string>(); 
+                bool isDone {(done == "true") ? false : true  };
+
+                Task newTask = Task(title,content,isDone);
+                newTodo.addTask(newTask);
+
+
+            }
+
+            data.push_back(newTodo);
+        }
+        
         return data;
     }
     void writeFile(std::vector<Todo> todos, std::string path)
     {
         YAML::Emitter output; //this will output our data into our data file
         
-        for (Todo& todo: todos)
+        output << YAML::BeginSeq;
+
+        
+        int counter{0} ;
+        for(Todo& t : todos )
         {
-            output << YAML::BeginMap;
-
-            output << YAML::Key   << "Title";
-            output << YAML::Value << todo.getName();
-            output << YAML::Key   << "Status";
-            output << YAML::Value << status(todo.getStatus());
-            
-            int counter {0};
-            //now loop through all our tasks
-            for (Task& task : todo.getTasks())
+             output << YAML::BeginMap;
+             output << YAML::Key << counter;
+             output << YAML::Value << t.getName();
+             output << YAML::Key << "status";
+             output << YAML::Value << status(t.getStatus());
+             output << YAML::Key << "data";
+             output << YAML::Value;
+             output << YAML::BeginSeq;
+             
+            for (Task& tsk : t.getTasks())
             {
-                 std::string header {"Task "};
-                 header += std::to_string(counter);
-                 output << YAML::Key   << header ;
-                 output << YAML::Value 
-                        << YAML::BeginSeq
-                        << task.getName()
-                        << task.getContent()
-                        << task.getStatus()
-                        << YAML::EndSeq;
-
-                 ++counter;
-                                    
+                output << YAML::BeginMap;
+                output << YAML::Key   << "name";
+                output << YAML::Value << tsk.getName();
+                output << YAML::Key   << "content";
+                output << YAML::Value << tsk.getContent();
+                output << YAML::Key   << "isDone" << YAML::Value << tsk.getStatus();
+                output << YAML::EndMap;
             }
-            output << YAML::EndMap; 
+            output << YAML::EndSeq;
+            output << YAML::EndMap;
+            ++counter;
         }
+        output << YAML::EndSeq;
 
         //overwrite old data in the data.yaml file with the new data
         std::ofstream file;

df0419255758bf3b03a8fbf0d3c80709a5771685
haydn <haydnt@zohomail.com>
2022-09-13 16:17:39 -0700

    runs ncurses stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "ncurses.h"
 #include "yaml-cpp/yaml.h"
-#include "types.h"
-#include "file.h"
+#include "run.h"
 int main()
 {	
 	//initscr();			/* Start curses mode 		  */
@@ -9,27 +8,9 @@ int main()
 //	refresh();			/* Print it on to the real screen */
 //	getch();			/* Wait for user input */
 //	endwin();			/* End curses mode		  */
-   
-    std::vector<Todo> todos;
-    std::string path {"/home/haydn/Github/TodoList/test/test.yaml"};
-    Todo test = Todo("A");
-    Todo test2 = Todo("Todo");
-    Todo test3 = Todo("test");
     
-    Task t = Task("test", "Test code for bugs");
-    Task y = Task("r", "There are bugs in the code");
+    run();
 
-    test.addTask(t);
-    test2.addTask(t);
-    test2.addTask(y);
-    test3.addTask(t);
-
-    todos.push_back(test);
-    todos.push_back(test2);
-    todos.push_back(test3);
-    todos.push_back(test);
-    datastream::writeFile(todos, path); 
-    datastream::writeFile(datastream::readFile(path), "/home/haydn/Github/TodoList/test/test3.yaml");
     return 0;
 }
 

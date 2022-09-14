b27eda50aea52af29f62a9241641863ef223019b
trolllemon <haydnt@zohomail.com>
2022-09-13 17:41:32 -0700

    getLongestLength declaration

--- src/file.h
+++ src/file.h
@@ -3,7 +3,8 @@
 #include "types.h"
 #include <vector>
 namespace datastream {
- 
+    
+    int getLongestLength(std::vector<Todo> todos);
     std::vector<Todo> readFile(std::string& path);//reads the file and returns all Todo data
     void writeFile(std::vector<Todo> todos, std::string path);//writes data in memory to the yaml file
     void createBackup(); //creates a backup of the data when it is loaded just in case changes get corrupted

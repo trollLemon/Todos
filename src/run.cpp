8760b12b2da7d3f3f7880a5e87a6c77fc3e990a5
trolllemon <haydnt@zohomail.com>
2022-09-13 19:56:35 -0700

    cleaned up code

--- src/run.cpp
+++ src/run.cpp
@@ -5,6 +5,16 @@
 #include "locale.h"
 #include <iostream>
 #include <string>
+#include <stdlib.h> 
+#include <bits/stdc++.h>
+
+
+
+
+/*Const variables-------------------------------------------------------------*/
+
+
+
 //logo
 const std::string logo1{"      #       # #######                             "};
 const std::string logo2{"     #       #     #     ####  #####   ####   ####  "};
@@ -14,32 +24,35 @@ const std::string logo5{"  #       #        #    #    # #    # #    #      # "};
 const std::string logo6{" #       #         #    #    # #    # #    # #    # "};
 const std::string logo7{"#       #          #     ####  #####   ####   #### "};
 
-std::string path {"/home/haydn/Github/TodoList/test/test.yaml"};
 
-//our text stuff
+std::string path {"/home/haydn/Github/TodoList/test/test.yaml"};
 const std::string mesg {" ~ A todo list from inside your Terminal ~"};
 const std::string header {"Todos:"};
-
-//window stuff
 const std::string keybinds {"F1:Exit|F2:Save|F3:Exit And Save|F4:Access Todos"};
+int logoSize {static_cast<int>(logo1.size())};
+int mesgSize {static_cast<int>(mesg.size())};
+int headerSize{static_cast<int>(header.size())};
 
+
+/*declarations--------------------------------------------------------------------------*/
 void printTodos(WINDOW* , std::vector<Todo>& todos);//prints todo titles in our little box below the "Todos:" header
 
 
-//TODO:organize this into less messy code
 void run()
 {
 
-    //before we do anything, lets load our data from the yaml file
+    
     std::vector<Todo> todos {datastream::readFile(path)};
     
-    int longestLength {datastream::getLongestLength(todos)+7};//the size for our todo box under the todos header,
-                                                              //we add 7 to it to add margins to the text
-    
-    int logoSize {static_cast<int>(logo1.size())};
-    int mesgSize {static_cast<int>(mesg.size())};
-    int headerSize{static_cast<int>(header.size())};
-    setlocale(LC_ALL, "");
+
+
+    /*runtime vars*/
+    int longestLength {datastream::getLongestLength(todos)+7};//the width for our todo box under the todos header,we add 7 so the text has some space from the borders
+    int todoSize {static_cast<int>(todos.size())+4};          //the height for the todo box
+                                                              
+
+    /*Init main window*/
+    setlocale(LC_ALL, "");  //set locale so we can print special chars 
 
     initscr();
     start_color();
@@ -47,10 +60,13 @@ void run()
     cbreak();
     keypad(stdscr,true);
     curs_set(0);
-
+    
+    //set up some colors
     init_pair(1, COLOR_CYAN, COLOR_BLACK);
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_MAGENTA, COLOR_BLACK);
+    
+    
     box(stdscr,0,0); 
 
     int row {};
@@ -58,7 +74,8 @@ void run()
 
     getmaxyx(stdscr,row,col);	
 
-    //print the logo art
+    /*Printing Main Text*/
+
     mvprintw((row-(row-3)),(col-logoSize)/2, "%s", logo1.c_str());
     mvprintw((row-(row-4)),(col-logoSize)/2, "%s", logo2.c_str());
     mvprintw((row-(row-5)),(col-logoSize)/2, "%s", logo3.c_str());
@@ -66,38 +83,32 @@ void run()
     mvprintw((row-(row-7)),(col-logoSize)/2, "%s", logo5.c_str());
     mvprintw((row-(row-8)),(col-logoSize)/2, "%s", logo6.c_str());
     mvprintw((row-(row-9)),(col-logoSize)/2, "%s", logo7.c_str());
-    //color the logo
+    
+    //color the ascii text
     for(int i{3}; i<10; ++i)
     {
         mvchgat((row-(row-i)), (col-logoSize)/2, logoSize, A_BOLD, 3, NULL);
     }
 
-
-    //print greeter
     mvprintw(row/2,(col-mesgSize)/2,"%s",mesg.c_str());
     mvchgat(row/2, (col-mesgSize)/2, mesgSize, A_BLINK, 1, NULL);	
-    //print header
     mvprintw((row/2)+4, (col-headerSize)/2, "%s", header.c_str());
     mvchgat((row/2)+4,(col-headerSize)/2, headerSize, A_BLINK,2,NULL);
     
-    //TODO:load todo data and init an int as the length of the vector, also do the same for the width by getting the length of the largest title
-
-    
-    int todoSize {static_cast<int>(todos.size())+4};
-    //make a window for the menu
-    WINDOW *win = newwin(todoSize,longestLength,(row/2)+6,(col-longestLength)/2);
-    box(win,0,0);
-
-    //print keybinds at the bottom of the screen
     mvprintw(row-3, (col-keybinds.size())/2, "%s", keybinds.c_str());
     mvchgat(row-3, (col-keybinds.size())/2, keybinds.size(), A_BOLD,2, NULL);
     refresh();
-    
+   
+    /*Other windows*/
+    WINDOW *win = newwin(todoSize,longestLength,(row/2)+6,(col-longestLength)/2);
+    box(win,0,0);
+
     printTodos(win,todos);
 
-    //mvwprintw(win, 0,0,"%s", "penis");
 
-    //wrefresh(win);
+    //TODO: wait for user input, if F4 is pressed, pass input into the second window so the user can select a Todo
+
+    
     getch();
     endwin();
 

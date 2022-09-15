2e2e626f44050d881ccc73493b138980d60dcde0
trolllemon <haydnt@zohomail.com>
2022-09-14 17:52:07 -0700

    removed todo intro comment

--- src/run.h
+++ src/run.h
@@ -1,32 +1,4 @@
 #ifndef NCURSES_RUN
 #define NCURSES_RUN
-/*
-
-                            _________ ___  ____   ___  
-                           / / /_   _/ _ \|  _ \ / _ \ 
-                          / / /  | || | | | | | | | | |
-                         / / /   | || |_| | |_| | |_| |
-                        /_/_/    |_| \___/|____/ \___/ 
-                                
-                       ~ A todo list from inside your Terminal ~
-
-
-
-
-                       todos:
-
-
-                       [*] a  Status: done :) 
-                       []  b  Status: in progress...
-                       []  c  Status: Not Started :O
-
-
-
-*/
-
-
-void run();
-
-
-
+int run();
 #endif
